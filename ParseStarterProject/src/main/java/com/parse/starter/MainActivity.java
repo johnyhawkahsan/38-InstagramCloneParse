8b6a37dc74f21be039ec920a9eadaed52872bb13
johnyhawk <johnyhawkahsan@gmail.com>
2017-08-24 11:17:18 +0500

    3rd Commit-All test code is moved to StarterApplication. Now, I'm starting building Instagram clone app.

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,9 +9,13 @@
 package com.parse.starter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
 
 import com.parse.FindCallback;
@@ -25,11 +29,14 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
 
 
   @Override
@@ -39,89 +46,6 @@ public class MainActivity extends AppCompatActivity {
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
-
-/*
-    //Save data in Parse Server
-    ParseObject score = new ParseObject("Score");
-    score.put("username", "Ahsan");
-    score.put("score", 86);
-
-    score.saveInBackground(new SaveCallback() {
-      @Override
-      public void done(ParseException e) {
-
-        if (e == null){
-
-          Log.i("SaveInBackground", "Successful");
-
-        } else {
-
-          Log.i("SaveInBackground", "Failed. Error: " + e.toString());
-
-        }
-
-      }
-    });
-*/
-
-/*
-    //Retrieve Data from parse
-    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
-
-    query.getInBackground("XNwMTiapPX", new GetCallback<ParseObject>() {
-        @Override
-        public void done(ParseObject object, ParseException e) {
-
-            if (e == null && object != null){
-
-                //If we want to update the score data, we can simply use this object to update
-                object.put("score", 200);
-                object.saveInBackground();//This time no call back
-
-                Log.i("username", object.getString("username"));
-                Log.i("score", String.valueOf(object.getInt("score")));
-
-            } else {
-
-                Log.i("ObjectRetrieveError", "Failed. Error: " + e.toString());
-
-            }
-
-        }
-    });
-*/
-
-    //Advance Parse Queries
-      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");//Score class
-
-      //Add a search criteria
-      query.whereEqualTo("username", "ralphie");
-      query.setLimit(1);//Return only one result
-
-      query.findInBackground(new FindCallback<ParseObject>() {
-          @Override
-          public void done(List<ParseObject> objects, ParseException e) {
-
-              //If there is no exception error, means everything worked
-              if (e == null){
-
-                Log.i("findInBackground", "Retrieved " + objects.size() + " objects");
-
-                if (objects.size() > 0){
-
-                    //We created ParseObject called object from each item in objects
-                    for (ParseObject object : objects){
-
-                        Log.i("findInBackgroundResult", object.toString());
-                        Log.i("findInBackgroundStrings", object.getString("username"));
-                        Log.i("findInBackgroundStrings", String.valueOf(object.getInt("score")));
-
-                    }
-
-                }
-              }
-          }
-      });
 
 
 
--- ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -56,7 +56,188 @@ public class StarterApplication extends Application {
     });
 */
 
-    ParseUser.enableAutomaticUser();
+
+     //TODO: All my test code is moved from MainActivity.java onCreate method to here
+
+      /*
+    //Save data in Parse Server
+    ParseObject score = new ParseObject("Score");
+    score.put("username", "Ahsan");
+    score.put("score", 86);
+
+    score.saveInBackground(new SaveCallback() {
+      @Override
+      public void done(ParseException e) {
+
+        if (e == null){
+
+          Log.i("SaveInBackground", "Successful");
+
+        } else {
+
+          Log.i("SaveInBackground", "Failed. Error: " + e.toString());
+
+        }
+
+      }
+    });
+*/
+
+/*
+    //Retrieve Data from parse
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+
+    query.getInBackground("XNwMTiapPX", new GetCallback<ParseObject>() {
+        @Override
+        public void done(ParseObject object, ParseException e) {
+
+            if (e == null && object != null){
+
+                //If we want to update the score data, we can simply use this object to update
+                object.put("score", 200);
+                object.saveInBackground();//This time no call back
+
+                Log.i("username", object.getString("username"));
+                Log.i("score", String.valueOf(object.getInt("score")));
+
+            } else {
+
+                Log.i("ObjectRetrieveError", "Failed. Error: " + e.toString());
+
+            }
+
+        }
+    });
+*/
+
+
+/*
+    //Advance Parse Queries-Where clause
+      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");//Score class
+
+      //Add a search criteria
+      query.whereEqualTo("username", "ralphie");
+      query.setLimit(1);//Return only one result
+
+      query.findInBackground(new FindCallback<ParseObject>() {
+          @Override
+          public void done(List<ParseObject> objects, ParseException e) {
+
+              //If there is no exception error, means everything worked
+              if (e == null){
+
+                Log.i("findInBackground", "Retrieved " + objects.size() + " objects");
+
+                if (objects.size() > 0){
+
+                    //We created ParseObject called object from each item in objects
+                    for (ParseObject object : objects){
+
+                        Log.i("findInBackgroundResult", object.toString());
+                        Log.i("findInBackgroundStrings", object.getString("username"));
+                        Log.i("findInBackgroundStrings", String.valueOf(object.getInt("score")));
+
+                    }
+
+                }
+              }
+          }
+      });
+*/
+
+
+/*
+      //Search when score is greater than 200 and add extra 50 points to it
+      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");//Score class
+      query.whereGreaterThan("score", 200);
+
+      query.findInBackground(new FindCallback<ParseObject>() {
+          @Override
+          public void done(List<ParseObject> objects, ParseException e) {
+
+              if (e == null && objects != null){
+
+                  for (ParseObject object: objects){
+
+                      object.put("score", object.getInt("score") + 50);//Get integer value in score and add 50
+                      object.saveInBackground();
+
+                  }
+
+              }
+
+          }
+      });
+*/
+
+/*
+      //Create a new user
+      ParseUser user = new ParseUser();
+
+      user.setUsername("ahsan");
+      user.setPassword("123");
+
+      user.signUpInBackground(new SignUpCallback() {
+          @Override
+          public void done(ParseException e) {
+
+              if (e == null){
+
+                  Log.i("Sign Up", "Successful");
+
+              } else {
+
+                  Log.i("Sign Up", "Failed. Error: " + e.toString());
+
+              }
+
+          }
+      });
+*/
+
+/*
+      ParseUser.logInInBackground("ahsan", "123", new LogInCallback() {
+          @Override
+          public void done(ParseUser user, ParseException e) {
+
+              if (e == null){
+
+                  Log.i("Log In", "Successful");
+
+              } else {
+
+                  Log.i("Log In", "Failed. Error: " + e.toString());
+
+              }
+
+          }
+      });
+*/
+
+
+/*
+      //Check if user is logged in
+      if(ParseUser.getCurrentUser() != null){
+
+          Log.i("currentUser", "User Logged in " + ParseUser.getCurrentUser().getUsername());
+
+      } else {
+
+          Log.i("currentUser", "User not logged in");
+
+      }
+*/
+
+
+/*
+      //Log out the current user
+      ParseUser.logOut();
+*/
+
+
+
+    //If you want to create automatic user
+    //ParseUser.enableAutomaticUser();
 
     ParseACL defaultACL = new ParseACL();
     defaultACL.setPublicReadAccess(true);
